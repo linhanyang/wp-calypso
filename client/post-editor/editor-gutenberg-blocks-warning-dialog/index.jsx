6aedcdbaa10d8a6a9b6ea53651201098d4521a26
Kirk Wight <kwight@kwight.ca>
2018-11-14 17:04:43 -0800

    Correct Google stats reference to the warning dialog.

--- client/post-editor/editor-gutenberg-blocks-warning-dialog/index.jsx
+++ client/post-editor/editor-gutenberg-blocks-warning-dialog/index.jsx
@@ -164,7 +164,7 @@ const mapDispatchToProps = dispatch => ( {
 				composeAnalytics(
 					recordGoogleEvent(
 						'Gutenberg Calypso Opt-In Dialog',
-						'Clicked "Use the classic editor" in the editor opt-in sidebar.',
+						'Clicked "Use the classic editor" in the blocks warning dialog.',
 						'Use-Classic',
 						true
 					),
